89a4c3eb422ae8e616f1b28760c3ab5e5117cc82
Kurt W <kurt.weston97@gmail.com>
2017-05-09 16:23:24 +1200

    Created Class DialogAction and fixed GUI bug
    DialogAction - contains all the actions that the buttons
    (settings,sync,about) the buttons will do. Works with both week and
    month views (these classes pass in a child of the AppCompatActivity
    class).
    
    GUI bug fixed was where each time a view was swapped, it would create a
    duplicate GUI overlaying the parent. This would lead to memory issues
    unless the app was forcibly closed. (This also rendered the exit button
    less than useful as you would have to exit n many times)

---
+++ Planner-Gui/app/src/main/java/com/kwrp/planner_gui/DialogAction.java
@@ -0,0 +1,84 @@
+package com.kwrp.planner_gui;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by KurtsPC on 9/05/2017.
+ */
+
+public class DialogAction {
+
+    public static AlertDialog createAboutDialog(AppCompatActivity parent) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(parent);
+        builder.setMessage("")
+                .setTitle("About us")
+                .setMessage("Cyka Blyat Idi Nahoi");
+
+        builder.setNeutralButton("Back", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+
+    public static AlertDialog createSettingsDialog(AppCompatActivity parent) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(parent);
+        builder.setMessage("")
+                .setTitle("Settings")
+                .setMessage("Change Colour scheme what else?");
+
+        builder.setNeutralButton("Back", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+
+    public static AlertDialog createSyncDialog(AppCompatActivity parent) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(parent);
+        builder.setMessage("")
+                .setTitle("Synchronise")
+                .setMessage("Synchronise with:\n-Evision\nGoogle Calendar\n-Hotmail Calendar\n-Facebook Calendar");
+
+        builder.setNeutralButton("Back", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+
+    public static AlertDialog exitButtonAction(AppCompatActivity parent) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(parent);
+        builder.setMessage("")
+                .setTitle("Exit")
+                .setMessage("Are you sure you want to exit?");
+
+        builder.setNeutralButton("Back", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNeutralButton("Exit", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+//                parent.finish();
+                System.exit(0);
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+
+
+
+
+}
