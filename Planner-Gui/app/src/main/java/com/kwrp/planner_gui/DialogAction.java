b99c39ca536faa62038dc930a5b0b7ad5abd3f7b
Kurt W <kurt.weston97@gmail.com>
2017-05-27 23:16:53 +1200

    front-end setup for multiple dates multiple events. Needs to be sent to back-end via JNI to be stored

--- Planner-Gui/app/src/main/java/com/kwrp/planner_gui/DialogAction.java
+++ Planner-Gui/app/src/main/java/com/kwrp/planner_gui/DialogAction.java
@@ -3,6 +3,15 @@ package com.kwrp.planner_gui;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * Created by KurtsPC on 9/05/2017.
@@ -44,7 +53,7 @@ public class DialogAction {
         AlertDialog.Builder builder = new AlertDialog.Builder(parent);
         builder.setMessage("")
                 .setTitle("Synchronise")
-                .setMessage("Synchronise with:\n-Evision\nGoogle Calendar\n-Hotmail Calendar\n-Facebook Calendar");
+                .setMessage("Synchronise with:\n-Evision\n-Google Calendar\n-Hotmail Calendar\n-Facebook Calendar");
 
         builder.setNeutralButton("Back", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
@@ -73,28 +82,80 @@ public class DialogAction {
         return dialog;
     }
 
-//    public static AlertDialog exitButtonAction(AppCompatActivity parent) {
-//        AlertDialog.Builder builder = new AlertDialog.Builder(parent);
-//        builder.setMessage("")
-//                .setTitle("Exit")
-//                .setMessage("Are you sure you want to exit?");
-//
-//        builder.setNeutralButton("Back", new DialogInterface.OnClickListener() {
-//            public void onClick(DialogInterface dialog, int id) {
-//                dialog.dismiss();
-//            }
-//        });
-//
-//        builder.setNeutralButton("Exit", new DialogInterface.OnClickListener() {
-//            public void onClick(DialogInterface dialog, int id) {
-////                parent.finish();
-//                System.exit(0);
-//            }
-//        });
-//
-//        AlertDialog dialog = builder.create();
-//        return dialog;
-//    }
+    public static AlertDialog createEventSetDialog(AppCompatActivity parent, Collection<Integer> positionList, String date) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(parent);
+        builder.setTitle("Create Event Set");
+
+        final LinearLayout dialogLayout = new LinearLayout(parent);
+        dialogLayout.setOrientation(LinearLayout.VERTICAL);
+
+        final TextView titleLabel = new TextView(parent);
+        titleLabel.setText("Event Title:");
+        dialogLayout.addView(titleLabel, 0);
+        final EditText titleInput = new EditText(parent);
+        titleInput.setInputType(InputType.TYPE_CLASS_TEXT);
+        dialogLayout.addView(titleInput, 1);
+
+        final TextView descriptLabel = new TextView(parent);
+        descriptLabel.setText("Event Description:");
+        dialogLayout.addView(descriptLabel, 2);
+        final EditText descriptInput = new EditText(parent);
+        descriptInput.setInputType(InputType.TYPE_CLASS_TEXT);
+        dialogLayout.addView(descriptInput, 3);
+
+        final TextView startLabel = new TextView(parent);
+        startLabel.setText("Start Time:");
+        dialogLayout.addView(startLabel, 4);
+        final EditText startInput = new EditText(parent);
+        startInput.setInputType(InputType.TYPE_CLASS_TEXT);
+        startInput.setHint("e.g. 0800");
+        dialogLayout.addView(startInput, 5);
+
+        final TextView durationLabel = new TextView(parent);
+        durationLabel.setText("Duration");
+        dialogLayout.addView(durationLabel, 6);
+        final EditText durationInput = new EditText(parent);
+        durationInput.setInputType(InputType.TYPE_CLASS_TEXT);
+        durationInput.setHint("Number of hours");
+        dialogLayout.addView(durationInput, 7);
+
+        builder.setView(dialogLayout);
+
+        //get month and year
+        String dateSuffix = "DialogAction.java ERROR";
+        for(int i =0;i<date.length(); i++){
+            if(date.charAt(i) == '/'){
+                dateSuffix = date.substring(i);
+                break;
+            }
+        }
+        ArrayList<String> dateList = new ArrayList<>();
+        for(Integer i : positionList) {
+            i -=6;
+            dateList.add(i.toString() + dateSuffix);
+        }
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String newEventTitle = ((EditText) dialogLayout.getChildAt(1)).getText().toString();
+                String newEventDescription = ((EditText) dialogLayout.getChildAt(3)).getText().toString();
+                String newEventStart = ((EditText) dialogLayout.getChildAt(5)).getText().toString();
+                String newEventDuration = ((EditText) dialogLayout.getChildAt(7)).getText().toString();
+                Log.d("details:", newEventTitle + newEventDescription + newEventStart + newEventDuration);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+
 
 
 
