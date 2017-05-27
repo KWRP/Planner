20104ee9a54e39b6b1a88055ecd994c8addac82d
Pomris <Zmitts13>
2017-05-27 17:22:23 +1200

    Renamed Native-lib

--- Planner-Gui/app/src/main/cpp/jni-dao.cpp
+++ Planner-Gui/app/src/main/cpp/jni-dao.cpp
@@ -1,6 +1,4 @@
 #include "include/xml-dao.hpp"
-#include "include/year.hpp"
-#include "include/day.hpp"
 #include <android/log.h>
 #include <jni.h>
 
@@ -10,55 +8,36 @@ JNIEXPORT jstring JNICALL
 Java_com_kwrp_planner_1gui_DisplayMonth_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "1";
-    return env->NewStringUTF(hello.c_str());
+    return env->NewStringUTF(NULL);
 }
 
 
 JNIEXPORT jstring JNICALL Java_com_kwrp_planner_1gui_DisplayMonth_testJNI(
         JNIEnv *env,
         jobject /* this */) {
-    Year year("29/10/1991");
-    std::string date = year.getDate();
-    return env->NewStringUTF(date.c_str());
+    return env->NewStringUTF(NULL);
 }
 
 
-// DisplayDay JNI calls
 
 JNIEXPORT jstring JNICALL Java_com_kwrp_planner_1gui_DisplayDay_jniGetCurrentDate(
         JNIEnv *env,
         jobject /* this */) {
-    Day *date = new Day();
-    return env->NewStringUTF((date->getDate()).data());
+    return env->NewStringUTF(NULL);
 }
-// DisplayDay JNI calls
 
 JNIEXPORT jstring JNICALL Java_com_kwrp_planner_1gui_DisplayMonth_jniGetCurrentDate(
         JNIEnv *env,
         jobject /* this */) {
-    Day *date = new Day();
-    return env->NewStringUTF((date->getDate()).data());
+    return env->NewStringUTF(NULL);
 }
-
 
 
 JNIEXPORT jstring JNICALL Java_com_kwrp_planner_1gui_DisplayDay_jniGetEvents(
         JNIEnv *env,
         jobject /* this */) {
-
-    std::string e = "Hello World";
-    return env->NewStringUTF(e.c_str());
+    return env->NewStringUTF(NULL);
 }
-
-JNIEXPORT jstring JNICALL Java_com_kwrp_planner_1gui_DisplayDay_jniGetDay(
-        JNIEnv *env,
-        jobject /* this */) {
-    Day *day = new Day();
-    std::string e = day->getDate();
-    return env->NewStringUTF(e.c_str());
-}
-
 
 JNIEXPORT jstring JNICALL Java_com_kwrp_planner_1gui_DisplayDay_jniCreateEvent(
         JNIEnv *env, jobject t,
@@ -69,17 +48,12 @@ JNIEXPORT jstring JNICALL Java_com_kwrp_planner_1gui_DisplayDay_jniCreateEvent(
     const char *nativeStart = env->GetStringUTFChars(start, 0);
     const char *nativeDuration = env->GetStringUTFChars(duration, 0);
     const char *nativePath = env->GetStringUTFChars(dir, 0);
+
+
     bool createFile = createXml(nativePath);
-    //bool checkDateCreated = checkDate(nativePath, "1", "1", "2017");
-
-    __android_log_print(ANDROID_LOG_INFO, "TEST C++!!!", "jniCreateEvent: %s -nativeTitle", nativeTitle);
-    __android_log_print(ANDROID_LOG_INFO, "TEST C++!!!", "jniCreateEvent: %s -nativeDescription", nativeDescription);
-    __android_log_print(ANDROID_LOG_INFO, "TEST C++!!!", "jniCreateEvent: %s -nativeStart", nativeStart);
-    __android_log_print(ANDROID_LOG_INFO, "TEST C++!!!", "jniCreateEvent: %s -nativeDuration", nativeDuration);
-
     bool createEvent = addEvent(nativePath, "10", "10", "2017", nativeTitle,
                                 nativeDescription, nativeStart, nativeDuration);
-
+    bool cd = checkDate(nativePath, "10", "10", "2017");
 
     (env)->ReleaseStringUTFChars(title, nativeTitle);
     (env)->ReleaseStringUTFChars(description, nativeDescription);
@@ -89,12 +63,26 @@ JNIEXPORT jstring JNICALL Java_com_kwrp_planner_1gui_DisplayDay_jniCreateEvent(
 
 
     std::string confirm = "";
-    if (createEvent) {
+    if (cd) {
         confirm = "Event Created!!";
     } else {
         confirm = "Event Creation Failed!!!";
     }
 
     return env->NewStringUTF(confirm.c_str());
+}
+
+JNIEXPORT jstring JNICALL Java_com_kwrp_planner_1gui_DisplayDay_jniGetDay(
+        JNIEnv *env,
+        jobject obj, jstring dir) {
+
+    const char *nativePath = env->GetStringUTFChars(dir, 0);
+
+    __android_log_print(ANDROID_LOG_INFO, "TEST C++!!! GetDay: ", "before");
+    Day *day = new Day("10/10/2017", nativePath);
+
+    (env)->ReleaseStringUTFChars(dir, nativePath);
+    __android_log_print(ANDROID_LOG_INFO, "TEST C++!!! GetDay", "%s", day->toString().c_str());
+    return env->NewStringUTF(day->toString().c_str());
 }
 }
