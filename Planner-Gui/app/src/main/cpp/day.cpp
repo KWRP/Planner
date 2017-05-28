7252d1bcb827ba1599f1b5e8eff5606bcde6390d
paulsouter <paul.souter@hotmail.co.nz>
2017-05-29 08:46:23 +1200

    Update day.cpp

--- Planner-Gui/app/src/main/cpp/day.cpp
+++ Planner-Gui/app/src/main/cpp/day.cpp
@@ -7,7 +7,6 @@
 
 #include "include/day.hpp"
 #include "include/event.hpp"
-#include <android/log.h>
 
 Day::Day(std::string date1, const char *filepath){
     this->date = date1;
@@ -18,10 +17,6 @@ Day::Day(std::string date1, const char *filepath){
     std::string rest1 = date.substr(date.find(delimiter) + 1);
     std::string month1 = rest1.substr(0, date.find(delimiter));
     std::string year1 = rest1.substr(date.find(delimiter) + 1);
-
-    __android_log_print(ANDROID_LOG_INFO, "TEST C++!!! Day constructor", "%s :%s :%s :%s : %s",
-                        this->date.c_str(), this->filepath, day1.c_str(), month1.c_str(),
-                        year1.c_str());
 
     bool getDay = pullDay(this, filepath, day1.c_str(), month1.c_str(), year1.c_str());
 }
@@ -48,19 +43,16 @@ Day::Day(const char* filepath) {
     pullDay(this, filepath, day, month, year);
 }
 
-Day::~Day() {
-	std::cout << "day of " + date + " destructor was called" << std::endl;
-}
-
 Event* Day::getEvent(int eventNumber) {
-	return events.at(eventNumber-1);
+    unsigned number = eventNumber -1;
+    return events[number];
 
 }
 
 std::string Day::toString() {
 	std::string result = "";
 	for(int i = 0; i < events.size(); i++){
-        result += numToString(i);
+        result += numToString(i + 1);
         result+= events[i]->toString();
     }
 	return result;
@@ -68,8 +60,16 @@ std::string Day::toString() {
 void Day:: setEvent(int id, std::string title, std::string description, int startTime, int duration) {
     Event *eventx = new Event(title, description, startTime, duration);
     events.push_back(eventx);
+
 }
-void Day::addEvent( std::string title, std::string description, int time, int duration) {//will change when the xml for event is working
+void Day::addEvent( std::string title, std::string description, int time, int duration) {
 	Event *eventx = new Event(filepath, date, title, description, time, duration);
 	events.push_back(eventx);
+}
+
+Day::~Day(){
+    int size = events.size();
+    for(int i=size; i> 0; i--){
+        free(events[i]);
+    }
 }
