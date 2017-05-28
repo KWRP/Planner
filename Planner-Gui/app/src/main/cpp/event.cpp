2b2df3f65e6f806baf28776cacd6a96cae6498bc
paulsouter <paul.souter@hotmail.co.nz>
2017-05-29 08:28:56 +1200

    Update event.cpp

--- Planner-Gui/app/src/main/cpp/event.cpp
+++ Planner-Gui/app/src/main/cpp/event.cpp
@@ -1,42 +1,26 @@
 #include "include/event.hpp"
 
 Event::Event(const char *filepath, std::string date, std::string title, std::string description,
-             int startTime, int duration) {//will change when the xml for event is working
- //   char* filepath = new char[100];
-  //  strcpy(filepath, "/Users/paulo/Documents");
-	this->description = description;
-	this->title = title;
-	this->startTime = startTime;
-	this->duration = duration;
-    char *titlec = new char[100];
-    strcpy(titlec, title.c_str());
-    char* descriptionc = new char[1000];
-    strcpy(descriptionc, description.c_str());
-    char *startTimec = new char[5];
-    strcpy(startTimec, numToString(startTime).c_str());
-    char* durationc = new char[9];
-    strcpy(durationc, numToString(duration).c_str());
-
-    char* day = new char[3];
-    strcpy(day, date.substr(0,2).c_str());
-    char* month = new char[3];
-    strcpy(month, date.substr(3,2).c_str());
-    char *year = new char [5];
-    strcpy(year, date.substr(6,4).c_str());
-	if(checkDate(day, month, year)){
-		addEvent( filepath,day, month, year, titlec, descriptionc, startTimec, durationc);
-	}
-	else{
-		createDate(day, month, year);
-        addEvent(filepath, day, month, year, titlec, descriptionc, startTimec, durationc);
-	}
-    free(day);
-    free(month);
-    free(year);
-    free(titlec);
-    free(descriptionc);
-    free(startTimec);
-    free(durationc);
+             int startTime, int duration) {
+    this->description = description;
+    this->title = title;
+    this->startTime = startTime;
+    this->duration = duration;
+    std::string time = numToString(startTime);
+    std::string durations = numToString(duration);    const char* month = date.substr(3,2).c_str();
+   
+ 
+    const char* day = date.substr(0,2).c_str();
+    const char* month = date.substr(3,2).c_str();
+    const char *year =date.substr(6,4).c_str();
+    if(checkDate(day, month, year)){
+        bool q = addEvent( filepath,day, month, year, title.c_str(), description.c_str(), time.c_str(), durations.c_str());
+    }
+    else{
+        bool x = createDate(day, month, year);
+        bool y = addEvent(filepath, day, month, year, title.c_str(), description.c_str(), time.c_str(), durations.c_str());
+    }
+        
 }
 Event::Event(std::string title, std::string description, int startTime, int duration){
 
@@ -48,6 +32,6 @@ Event::Event(std::string title, std::string description, int startTime, int dura
 std::string Event::toString() {
 	std::string result = "";
 	result = ";" + title + ";" + description + ";" +
-			numToString(startTime) + ";"+ numToString(duration) +":";//will change when the xml for event is working
+			numToString(startTime) + ";"+ numToString(duration) +":";
 	return result;
 }
